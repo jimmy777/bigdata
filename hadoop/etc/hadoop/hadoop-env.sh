baa60af98ef7f5910bcbc4c8634790ae3b8a690e
YuanZiYang <jimmy_777@126.com>
2020-02-04 17:19:46 +0800

    update 6 hadoop etc config files.

--- hadoop/etc/hadoop/hadoop-env.sh
+++ hadoop/etc/hadoop/hadoop-env.sh
@@ -22,7 +22,7 @@
 # remote nodes.
 
 # The java implementation to use.
-export JAVA_HOME=${JAVA_HOME}
+export JAVA_HOME=/opt/jdk1.8.0_211
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
